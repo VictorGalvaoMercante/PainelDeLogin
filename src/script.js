22f28df1ed8d330b81b1203fb0f69be48499e1f2
GalvasDev <146465630+VictorGalvaoMercante@users.noreply.github.com>
2023-10-27 15:13:51 -0300

    Projeto Painel de Login

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function mostrarSenha() {
+    var senha = document.getElementById("senha");
+    if (senha.type === "password") {
+      senha.type = "text";
+    } else {
+      senha.type = "password";
+    }   
+}   
+
+  
